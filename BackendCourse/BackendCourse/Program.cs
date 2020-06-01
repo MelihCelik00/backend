2309c0077f8ab67e5f8b5b3fe48eeae0f1e0784a
Melih Safa Çelik <45492251+MelihCelik00@users.noreply.github.com>
2020-06-01 11:45:37 +0300

    First homework-started with basics

---
+++ BackendCourse/BackendCourse/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace BackendCourse
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            bool nameCorrect = false;
+            bool surnameCorrect = false;
+            bool successfulEntry = false;
+
+            // Our little database(!)
+            string[] names = {"Melih","Safa","Ahmet","Mehmet","Veli"};
+            string[] surnames = new string[] {"Celik","Yilmaz","Torun","Ozefe","Yugrum"};
+
+            // Login Operations
+            while (successfulEntry==false)
+            {
+                Console.WriteLine("Welcome to Login Page!\nEnter Your Name: ");
+                string nameInput = Console.ReadLine();
+                Console.WriteLine($"Welcome {nameInput}, Please Enter Your Surname: ");
+                string surnameInput = Console.ReadLine();
+
+                for (int i = 0; i < names.Length; i++)
+                {
+                    if (names[i] == nameInput)
+                    {
+                        nameCorrect = true;
+                    }
+
+                    if (surnames[i] == surnameInput)
+                    {
+                        surnameCorrect = true;
+                    }
+                }
+
+                if (nameCorrect == true && surnameCorrect == true)
+                {
+                    Console.WriteLine($"Successful login! Welcome {nameInput} {surnameInput}");
+                    successfulEntry = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid name or surname entry please try again!");
+                }
+            }
+            Console.WriteLine("Have fun at rest of the program!! Good day! ");
+
+
+        }
+    }
+}
