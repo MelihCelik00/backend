ea79de60af75d5f5116df72d88834107ff98adb8
Melih Safa Çelik <45492251+MelihCelik00@users.noreply.github.com>
2020-06-10 20:37:34 +0300

    Bugs fixed, program ends

--- RegisterLogin/RegisterLogin/Enter.cs
+++ RegisterLogin/RegisterLogin/Enter.cs
@@ -5,54 +5,35 @@ namespace RegisterLogin
 {
     public class Register
     {
-        private List<Register> userInfo = new List<Register>();
-        private string _userName { get; set; }
-        private string _password { get; set; }
-        private string _email { get; set; }
-        private string _bod { get; set; }
-        /*
+
+        public string _userName;
+        public string _password;
+        public string _email;
+        public string _bod;
+       
         public Register(string username,
                         string pwd,
                         string e_mail,
                         string birthday)
         {
-            userInfo.Add(new Register(_userName, _password,
-                                      _email, _bod)
-            {
-                _userName = username,
-                _password = pwd,
-                _email = e_mail,
-                _bod = birthday,
-            });
+
+            _userName = username;
+            _password = pwd;
+            _email = e_mail;
+            _bod = birthday;
+          
         }
-        */
-
-        public List<Register> GetList() => userInfo;
-
-        public void AddList(string username, // Function is working!
-                        string pwd,
-                        string e_mail,
-                        string birthday)
+        public override string ToString()
         {
-            userInfo.Add(new Register()
-            {
-                _userName = username,
-                _password = pwd,
-                _email = e_mail,
-                _bod = birthday,
-            });
-            Console.WriteLine("You are now registered to our system!");
+            return _userName + _password + _email + _bod;
         }
-
-        public bool CheckUser(string _usenam, string _password)
-        {
-            string name = _usenam;
-            string sifre = _password;
-            bool isOk = userInfo.Contains(new Register { _userName = name, _password = sifre });
-
-            if (isOk == true) { return true; }
-            else if (isOk == false) { return false; }
-            return false;
-        }
+        
+        //public void printOut()
+        //{
+        //    foreach (Register i in db)
+        //    {
+        //        Console.WriteLine(i);
+        //    }
+        //}
     }
 }
--- RegisterLogin/RegisterLogin/Program.cs
+++ RegisterLogin/RegisterLogin/Program.cs
@@ -3,59 +3,48 @@ using System.Collections.Generic;
 
 namespace RegisterLogin
 {
-    class MainClass : Register
+    class MainClass
     {
         public static void Main(string[] args)
         {
-            //List<Users> userInfo = new List<Users>();
-            /* 
-            Ödevimiz 
-            Register Login sistemi class yapısı ile
+            Database db = new Database();
 
-            Programdan beklenti
-            kullanıcı register olacak
-            Register da kullanıcı adı şifre email doğum tarihi alınacak
-            Login ile kullanıcı adı ve şifre ile giriş yapacak
-            Sonsuz loop içinde yapabilirsiniz bu ekranı
-            Console da 1 e tıkladığımda login e gidecek
-            2 ye tıklarsam register a gidecek
-
-            Login olduktan sonra tekrar ana ekrana dönsün,register içinde aynı şekilde 
-            */
+            //x.RegisterFunc("ahmet", "123", "melihsafa.cgmail.com", "23082000");
+            //x.Login("melih", "123");
+            //x.RegisterFunc("melih", "123", "melihsafa.cgmail.com", "23082000");
+            //x.Login("melih","123");
+            
+            //ıList<Register> db = new List<Register>();
 
             Console.WriteLine("Welcome to our program! Let's continue.");
             while (true)
             {
-                Console.WriteLine("Please enter 1 to login, enter 2 to register.");
+                Console.WriteLine("Please enter 1 to login, enter 2 to register.\nPress 0 to exit.");
                 int x = Convert.ToInt32(Console.ReadLine());
+                if(x == 0 ) return;
                 if(x == 1)// login => username and password
                 {
                     Console.WriteLine("\nWelcome to the login page!\n\n");
                     bool Correction = false;
                     while (Correction == false)
                     {
-
-
                         Console.WriteLine("Enter your username: ");
                         string userN = Console.ReadLine();
 
                         Console.WriteLine("Enter your password: ");
                         string userP = Console.ReadLine();
 
-                        Register log = new Register();
-                        if (log.CheckUser(userN, userP) == true)
+                        
+                        if (Correction=db.Login(userN, userP)==true)
                         {
-                            Console.WriteLine($"Welcome {userN}, you are good to go!");
-                            Correction = true;
+                            Console.WriteLine("Successful login!!!!\n");
+                            //Correction = true;
                         }
                         else
                         {
-                            Console.WriteLine("Your username or password is wrong, please enter again!");
+                            Console.WriteLine("Invalid input, try again.");
                         }
-
                     }
-
-
                 }
                 else if (x==2)// register
                 {
@@ -69,14 +58,15 @@ namespace RegisterLogin
                     Console.WriteLine("\nPlease enter your date of birth: ");
                     string BirthDate = Console.ReadLine();
 
-                    Register reg = new Register();
-                    reg.AddList(usName, NewPassword, RegisteringMail, BirthDate);
+                    db.RegisterFunc(usName, NewPassword, RegisteringMail, BirthDate);
+                    
                 }
                 else
                 {
                     Console.WriteLine("Invalid Input!!!!");
                 }
             }
+            
         }
     }
 }
